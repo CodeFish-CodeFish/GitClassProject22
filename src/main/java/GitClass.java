e784eaa1430ac7deddbcce0c019f12cb7b2810a2
codefish <contact@codefish.io>
2024-08-05 20:07:04 -0500

    stashed changes

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -2,5 +2,7 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("From bob branch");
+        System.out.println("stash");
+
     }
 }
