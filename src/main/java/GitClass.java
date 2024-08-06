8e44cbd7771e2b989fa20a2d13e96fc1cc7d7c44
codefish <contact@codefish.io>
2024-08-05 19:20:16 -0500

    commit from new branch

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -1,5 +1,8 @@
 public class GitClass {
     public static void main(String[] args) {
-        System.out.println("Project git");
+
+        System.out.println("Some errors ");
+        // some bad codes
+
     }
 }
